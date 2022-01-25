c631ed806ee94c758e45ea234558e5765ff04e57
Gorgepycode <gorgepycode@gmail.com>
2022-01-25 22:54:06 +0300

    the types of loops

---
+++ learning_dart/bin/challinge/cha3.dart
@@ -0,0 +1,9 @@
+import 'dart:math';
+
+void main() {
+  const numbers = [1, 2, 4, 7];
+  // for (var number in numbers) {
+  //   print(sqrt(number));
+  // }
+  numbers.forEach((number) => print(sqrt(number)));
+}
