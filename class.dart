5e905be5183d13eb8e1bb0df795d77fdb6776622
Gorgepycode <gorgepycode@gmail.com>
2022-02-26 23:37:22 +0300

    Adding class file for dart tutorial

--- class.dart
+++ class.dart
@@ -2,6 +2,10 @@ class User {
   int id = 0;
   String name = '';
 
+  String toJson() {
+    return '{"id":$id, "name": $name}';
+  }
+
   @override
   String toString() {
     return 'User(id: $id, name: $name)';
